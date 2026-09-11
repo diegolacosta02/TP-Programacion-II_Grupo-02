67934b9d99a0fb678a77b82451d0c40e097d6636
GONZALOFLEITA <gonzalofleita19@gmail.com>
2026-09-11 17:49:31 -0300

    Se crea una parte de la clase restaurante

---
+++ src/restaurante.ts
@@ -0,0 +1,31 @@
+export class Restaurante {
+
+    private historialPedidos[]: Pedido
+    private menu: Menu[]
+    private estaciones: EstacionDeTrabajo[]
+    // menuInicial = productos y combos 
+    // estaciones = estaciones de cocina disponibles
+    constructor(estaciones: EstacionDeTrabajo[], menuInicial: Menu[] = [] ){
+        this.menu = menuInicial;
+        this.estaciones = estaciones;
+        this.historialPedidos = []
+    }
+
+    public registrarPedido(pedido: Pedido) : void {
+        this.historialPedidos.push(pedido); // Registra un nuevo pedido y lo agrega al historial
+    }
+
+
+    public agregarItem (pedido: Pedido, item: ItemPedido) : void{
+        pedido.agregarItem(item); // Agrega un item pedido
+    }
+
+    public quitarItem(pedido: Pedido) : void {
+        pedido.quitarUltimoItem(); // Elimina el ultimo item agregado a un pedido
+    }
+
+    public confirmarPedido(pedido: Pedido) : void {
+        pedido.confirmar();
+    }
+
+}
